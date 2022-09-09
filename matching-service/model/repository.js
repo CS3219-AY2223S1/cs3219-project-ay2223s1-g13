060962c15e3dd141798873489c03819b3b2531df
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-09-09 21:40:14 +0800

    ✨ add: repository code for creating a match

--- matching-service/model/repository.js
+++ matching-service/model/repository.js
@@ -1,25 +1,11 @@
 import MatchModel from './match-model.js';
-import 'dotenv/config'
+import { Sequelize } from 'sequelize';
 
-//Set up mongoose connection
-import mongoose from 'mongoose';
+const sequelize = new Sequelize('sqlite::memory:');
 
-let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-let db = mongoose.connection;
-db.on('error', console.error.find(console, 'MongoDB connection error:'));
+const matchModel = await MatchModel(sequelize);
+await matchModel.sync({ force: true });
 
 export async function createMatch(params) {
-    return new MatchModel(params)
-}
-
-export async function findMatch(timing) {
-    //logic to filter first user with timing +-30secs
-    return;
-}
-
-export async function clearMatch(user) {
-    //remove user from match collection (means no existing sess/matching for this user)
-    return;
+  return matchModel.create(params);
 }
