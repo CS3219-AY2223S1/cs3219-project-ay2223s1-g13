0ba952e4582045b174e54cbdc0647468daaa9de7
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-09-09 21:44:41 +0800

    ✨ add: create match function in orm

--- matching-service/model/match-orm.js
+++ matching-service/model/match-orm.js
@@ -1,10 +1,11 @@
+import { createMatch } from "./repository.js";
 
-export async function ormCreateMatch(user, difficulty, time) {
+export async function ormCreateMatch(user1, user2, createdAt, otherprops) {
     try {
+        await createMatch({ user1, user2, createdAt, otherprops });
         return true;
-
     } catch (err) {
-        console.log('ERROR: Could not create matching');
+        console.log("ERROR: Could not create new match");
         return { err };
     }
 }
