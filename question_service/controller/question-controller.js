e8f4e96c135c6510f2c811e332191e3c6016381d
Sourabh Jaiswal <sourabhrj31@gmail.com>
2022-09-25 18:51:05 +0800

    🍱 assets: create sections for questions at room page

--- question_service/controller/question-controller.js
+++ question_service/controller/question-controller.js
@@ -1,34 +1,51 @@
-import { ormCreateQuestion as _createQuestion, ormFindQuestion as _findQuestion } from "../model/question-orm.js";
-
+import {
+    ormCreateQuestion as _createQuestion,
+    ormFindQuestion as _findQuestion,
+} from "../model/question-orm.js";
 
 export async function createQuestion(req, res) {
     try {
         const { title, body, difficulty } = req.body;
-        if( title && body && difficulty ) {
-            const resp = await _createQuestion(title, body, difficulty)
-            if(resp.err) {
-                return res.status(400).json({ message: 'Could not create a new question'})
+        if (title && body && difficulty) {
+            const resp = await _createQuestion(title, body, difficulty);
+            if (resp.err) {
+                return res
+                    .status(400)
+                    .json({ message: "Could not create a new question" });
             } else {
-                return res.status(201).json({ message: `Create the question ${title} succesfully!`})
+                return res.status(201).json({
+                    message: `Create the question ${title} succesfully!`,
+                });
             }
         } else {
-            return res.status(400).json({ message: "Incomplete question format! "})
+            return res
+                .status(400)
+                .json({ message: "Incomplete question format! " });
         }
     } catch (err) {
-        return res.status(500).json({ message: 'Database failure when creating new question'})
+        return res
+            .status(500)
+            .json({ message: "Database failure when creating new question" });
     }
 }
 
 export async function findQuestion(req, res) {
     try {
-        const {difficulty} = req.body;
-        if(difficulty) {
+        console.log(req);
+        const { difficulty } = req.query;
+        if (difficulty) {
             const question = await _findQuestion(difficulty);
-            return res.status(202).json({message: "Found a question", question: question})
+            return res
+                .status(202)
+                .json({ message: "Found a question", question: question });
         } else {
-            return res.status(400).json({ message: "Enter the difficulty level for the problem"})
+            return res.status(400).json({
+                message: "Enter the difficulty level for the problem",
+            });
         }
     } catch {
-        return res.status(500).json({ message: "Server error when finding a question!" })
+        return res
+            .status(500)
+            .json({ message: "Server error when finding a question!" });
     }
 }
--- question_service/model/repository.js
+++ question_service/model/repository.js
@@ -1,19 +1,25 @@
-import QuestionModel from "./question-model.js"
-import 'dotenv/config'
+import QuestionModel from "./question-model.js";
+import "dotenv/config";
 
 // Set up mongoose connection
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-let mongoDB = process.env.ENV == "PROD" ? process.env.DB_CLOUD_URI : process.env.DB_LOCAL_URI;
+let mongoDB =
+    process.env.ENV == "PROD"
+        ? process.env.DB_CLOUD_URI
+        : process.env.DB_LOCAL_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 export async function createQuestion(params) {
-    return new QuestionModel(params)
+    return new QuestionModel(params);
 }
 
 export async function findQuestion(difficulty) {
-    return QuestionModel.findOne({difficulty})
+    return QuestionModel.aggregate([
+        { $match: { difficulty: difficulty } },
+        { $sample: { size: 1 } },
+    ]);
 }
