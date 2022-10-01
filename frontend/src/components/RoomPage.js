c65e8aa2464c4673c8d7bec539a411b1f051a979
Bryan Ong <bryan.o@u.nus.edu>
2022-10-01 15:35:32 +0800

    Add confirmation dialog before exiting room

--- frontend/src/components/RoomPage.js
+++ frontend/src/components/RoomPage.js
@@ -1,7 +1,11 @@
 import {
+    Button,
+    Dialog,
+    DialogContent,
     Grid,
     IconButton,
     TextField,
+    Stack,
     Typography
 } from "@mui/material";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
@@ -9,6 +13,7 @@ import Question from "./Question/Question";
 import io from 'socket.io-client';
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Box } from "@mui/system";
 
 function RoomPage() {
     const socket = io('http://localhost:8003');
@@ -16,6 +21,8 @@ function RoomPage() {
 
     const [userCode, setUserCode] = useState(""); 
     const [isFirstConnect, setIsFirstConnect] = useState(true);
+    const [exitDialogOpen, setExitDialogOpen] = useState(false);
+    const [partnerExitedDialogOpen, setPartnerExitedDialogOpen] = useState(false);
 
     useEffect(() => {
         if (isFirstConnect) {
@@ -33,8 +40,7 @@ function RoomPage() {
     });
 
     socket.on("partner exit", () => {
-        sessionStorage.removeItem('roomId');
-        navigate('/home');
+        setPartnerExitedDialogOpen(true);
     });
 
     const sendToSocket = () => {
@@ -43,25 +49,53 @@ function RoomPage() {
         socket.emit("send code", {roomId: sessionStorage.getItem("roomId"), text: code}) 
     }; 
 
-    const handleExit = () => {
+    const handleFirstExit = () => {
         socket.emit("exit", {roomId: sessionStorage.getItem("roomId")});
         sessionStorage.removeItem('roomId');
         navigate('/home');
     }
 
+    const handleSecondExit = () => {
+        sessionStorage.removeItem('roomId');
+        navigate('/home');
+    }
+
     return (
-        <Grid container spacing={5}>
-            <Grid item>
-                <IconButton onClick={handleExit}>
-                    <ArrowBackIosIcon/>
-                    <Typography variant='h5'>Exit</Typography>
-                </IconButton>
-                <Question/>
+        <Box>
+            <Grid container spacing={5}>
+                <Grid item>
+                    <IconButton onClick={() => setExitDialogOpen(true)}>
+                        <ArrowBackIosIcon/>
+                        <Typography variant='h5'>Exit</Typography>
+                    </IconButton>
+                    <Question/>
+                </Grid>
+                <Grid item>
+                    <TextField multiline rows={20} value={userCode} id="textbox" variant="outlined" onChange={sendToSocket} style = {{width: 500}}/>
+                </Grid>
             </Grid>
-            <Grid item>
-                <TextField multiline rows={20} value={userCode} id="textbox" variant="outlined" onChange={sendToSocket} style = {{width: 500}}/>
-            </Grid>
-        </Grid>
+
+            <Dialog open = {exitDialogOpen} onClose = {() => setExitDialogOpen(false)}>
+                <DialogContent>
+                    <Stack display='flex' alignItems='center' spacing={2}>
+                        <Typography variant='body1'>Are you sure you want to exit?</Typography>
+                        <Stack direction='row' spacing={3}>
+                            <Button variant='contained' onClick={() => setExitDialogOpen(false)}>Cancel</Button>
+                            <Button variant='contained' onClick={handleFirstExit} color='error'>Exit</Button>
+                        </Stack>
+                    </Stack>
+                </DialogContent>
+            </Dialog> 
+
+            <Dialog open = {partnerExitedDialogOpen} onClose = {(e, r) => {if (r != "backdropClick") { navigate('/home') }}}>
+                <DialogContent>
+                    <Stack display='flex' alignItems='center' spacing={2}>
+                        <Typography variant='body1'>The other user has exited!</Typography>
+                        <Button variant='contained' onClick={handleSecondExit}>Exit</Button>
+                    </Stack>
+                </DialogContent>
+            </Dialog> 
+        </Box>
     );
 }
 
